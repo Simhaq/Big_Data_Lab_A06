8f41a320d619e7a046a5eea35df4c9406e40327c
Simhaq <msimhaqs1011@gmail.com>
2024-04-25 21:37:44 +0530

    Adding code for task2

---
+++ BDL6_task2.py
@@ -0,0 +1,74 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+from keras.models import load_model
+import numpy as np
+from PIL import Image
+import io
+
+app = FastAPI()
+
+def format_image(img):
+    
+    img = img.convert('L')  # Convert to grayscale
+
+    img = img.resize((28, 28))  # Resize the image to 28x28
+
+    # Convert image to numpy array
+    img_array = np.array(img)
+
+    # Reshape the data_point to match the input shape of the model
+    img_array = img_array.reshape(1,784)
+
+    return img_array
+
+def Load_Model(path: str):
+    """
+    Load the Keras Sequential model from the given path.
+    """
+    model = load_model(path)
+    return model
+
+def predict_digit(model, data_point):
+    """
+    Predict the digit using the loaded model.
+    """
+    # Normalize the data
+    data_point = data_point / 255.0
+    # Perform prediction
+    prediction = model.predict(data_point)
+    # Get the predicted digit
+    predicted_digit = np.argmax(prediction)
+    return str(predicted_digit)
+
+model_path = None
+
+@app.post('/predict')
+async def predict(upload_file: UploadFile = File(...)):
+    """
+    Predict the digit from the uploaded image file.
+    """
+    # Check if model is loaded
+    if model_path is None:
+        return {"error": "Model not loaded. Please load the model first."}
+    
+    # Read the uploaded image file
+    contents = await upload_file.read()
+    
+    img = Image.open(io.BytesIO(contents))         # Load the image 
+    
+    img_array = format_image(img)                  # To convert into greyscale and to serialize the image array into 784 elements
+    
+    # Call predict_digit function to get the predicted digit
+    digit = predict_digit(Load_Model(model_path), img_array)
+    
+    return {"Digit" : digit}
+    
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python app.py <model_path>")
+        sys.exit(1)
+    model_path = sys.argv[1]
+    
+    uvicorn.run(app)
